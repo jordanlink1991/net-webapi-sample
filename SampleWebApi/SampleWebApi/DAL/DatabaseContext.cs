76f0928549074dce581578c61e9619311e2b920b
Jordan Link <jordanlink1991@gmail.com>
2021-11-17 19:44:46 -0600

    Adding empty database context class

---
+++ SampleWebApi/SampleWebApi/DAL/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SampleWebApi.DAL
+{
+	public interface IDatabaseContext
+	{
+
+	}
+
+	public class DatabaseContext : DbContext, IDatabaseContext
+	{
+		#region Attributes
+		#endregion Attributes
+
+		#region Constructors
+		public DatabaseContext()
+			: base()
+		{
+		}
+		#endregion Constructors
+	}
+}
