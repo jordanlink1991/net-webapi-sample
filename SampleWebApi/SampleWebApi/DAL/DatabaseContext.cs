d9b06b759b140a9c5fd66e5e777f386757996ffd
Jordan Link <jordanlink1991@gmail.com>
2021-11-17 21:04:29 -0600

    Adding DbSet property with the Service Requets

--- SampleWebApi/SampleWebApi/DAL/DatabaseContext.cs
+++ SampleWebApi/SampleWebApi/DAL/DatabaseContext.cs
@@ -4,22 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using SampleWebApi.Models;
 
 namespace SampleWebApi.DAL
 {
 	public interface IDatabaseContext
 	{
-
+		#region Attributes
+		/// <summary>
+		/// List of service requests in the database
+		/// </summary>
+		DbSet<ServiceRequest> ServiceRequests { get; set; }
+		#endregion Attributes
 	}
 
 	public class DatabaseContext : DbContext, IDatabaseContext
 	{
 		#region Attributes
+		/// <summary>
+		/// List of service requests in the database
+		/// </summary>
+		public virtual DbSet<ServiceRequest> ServiceRequests { get; set; }
 		#endregion Attributes
 
 		#region Constructors
 		public DatabaseContext()
 			: base()
+		{
+		}
+
+		public DatabaseContext(DbContextOptions<DatabaseContext> options)
+			: base(options)
 		{
 		}
 		#endregion Constructors
