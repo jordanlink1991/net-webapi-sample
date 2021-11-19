a8fb94fe83c134bfaba4a317c73cb00c6436d56e
Jordan Link <jordanlink1991@gmail.com>
2021-11-18 20:33:45 -0600

    Adding mock Email utility

---
+++ SampleWebApi/SampleWebApi/EmailUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleWebApi.Models;
+
+namespace SampleWebApi
+{
+	public class EmailUtility
+	{
+		/// <summary>
+		/// Sends an email notifying that a request has been closed
+		/// </summary>
+		public static void SendEmailForClosedRequest(ServiceRequest request)
+		{
+			// Placeholder
+		}
+	}
+}
